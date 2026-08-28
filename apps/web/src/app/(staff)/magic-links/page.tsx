df2ee717b740a681b0fbc15a42ffb4cc2a000927
alexis <alexisboianelli@gmail.com>
2026-08-28 16:38:16 -0300

    feat(web): dashboard + magic links sections

---
+++ apps/web/src/app/(staff)/magic-links/page.tsx
@@ -0,0 +1,12 @@
+import { MagicLinksList } from './MagicLinksList';
+
+export default function MagicLinksPage() {
+  return (
+    <div>
+      <div className="staff-topbar">
+        <h2>Links mágicos</h2>
+      </div>
+      <MagicLinksList />
+    </div>
+  );
+}
